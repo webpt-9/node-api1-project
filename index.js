7a3262c9f33a8f15de54b9ddd43d61ca57a14020
andrew15722 <andrew15722@yahoo.com>
2020-01-15 21:42:49 -0700

    wrote all my endpoints and they work, met mvp goals

--- index.js
+++ index.js
@@ -92,11 +92,22 @@ server.delete('/api/users/:id', (req, res) => {
 
 server.put('/api/users/:id', (req, res) => {
 	const { id } = req.params;
-	const changes = req.body;
+	const user = req.body;
 
 	if (id) {
-		db.update(id, changes).then((updated) => {
-			res.status(200).json({ success: true, updated });
-		});
+		if (user.name && user.bio) {
+			db
+				.update(id, user)
+				.then((updated) => {
+					res.status(200).json({ success: true, updated });
+				})
+				.catch((err) => {
+					res.status(500).json({ error: 'The user information could not be modified' });
+				});
+		} else {
+			res.status(400).json({ error: 'please provide a name and bio for the user' });
+		}
+	} else {
+		res.status(404).json({ error: 'user with the specified id does not exist' });
 	}
 });
